4a8b3738bb6efd3cd015f8aa887e2aedc3fc0b1d
upiniano <2209105@upy.edu.mx>
2026-02-24 20:08:29 -0600

    Modified file spree_permissions.rb

--- config/initializers/spree_permissions.rb
+++ config/initializers/spree_permissions.rb
@@ -1,4 +1,10 @@
+# config/initializers/spree_permissions.rb
+
 Rails.application.config.after_initialize do
+  next if Rails.const_defined?(:Console)
+  next if defined?(Rails::Server).nil?
+  next if ENV["SECRET_KEY_BASE_DUMMY"]
+
   Spree::Role.find_or_create_by!(name: "store_owner")
 
   Spree.permissions.assign(:store_owner, [
