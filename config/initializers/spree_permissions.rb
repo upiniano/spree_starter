15177c156416ce24f089de26b024cf4de4ce5337
upiniano <2209105@upy.edu.mx>
2026-02-26 23:23:30 -0600

    Updated spre_permissions.rb after Spree deployment

--- config/initializers/spree_permissions.rb
+++ config/initializers/spree_permissions.rb
@@ -1,8 +1,12 @@
 # config/initializers/spree_permissions.rb
 
-Spree.permissions.assign(:store_owner, [
-  Spree::PermissionSets::DashboardDisplay,
-  Spree::PermissionSets::OrderManagement,
-  Spree::PermissionSets::ProductManagement,
-  Spree::PermissionSets::UserDisplay
-])
+Rails.application.config.to_prepare do
+  next unless defined?(Spree::PermissionSets)
+
+  Spree.permissions.assign(:store_owner, [
+    Spree::PermissionSets::DashboardDisplay,
+    Spree::PermissionSets::OrderManagement,
+    Spree::PermissionSets::ProductManagement,
+    Spree::PermissionSets::UserDisplay
+  ])
+end
