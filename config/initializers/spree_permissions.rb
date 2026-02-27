be6429ede6d7c2f3ed94914faf110deb82f66930
upiniano <2209105@upy.edu.mx>
2026-02-26 23:18:01 -0600

    Fixed spre_permissions.rb

--- config/initializers/spree_permissions.rb
+++ config/initializers/spree_permissions.rb
@@ -1,16 +1,8 @@
 # config/initializers/spree_permissions.rb
 
-Rails.application.config.after_initialize do
-  next if Rails.const_defined?(:Console)
-  next if defined?(Rails::Server).nil?
-  next if ENV["SECRET_KEY_BASE_DUMMY"]
-
-  Spree::Role.find_or_create_by!(name: "store_owner")
-
-  Spree.permissions.assign(:store_owner, [
-    Spree::PermissionSets::DashboardDisplay,
-    Spree::PermissionSets::OrderManagement,
-    Spree::PermissionSets::ProductManagement,
-    Spree::PermissionSets::UserDisplay
-  ])
-end
+Spree.permissions.assign(:store_owner, [
+  Spree::PermissionSets::DashboardDisplay,
+  Spree::PermissionSets::OrderManagement,
+  Spree::PermissionSets::ProductManagement,
+  Spree::PermissionSets::UserDisplay
+])
