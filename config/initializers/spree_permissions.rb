ac40272b03879fc60dfa837ffcce459f34d02def
upiniano <2209105@upy.edu.mx>
2026-03-12 12:53:54 -0600

    test: Testing store owner permissions

--- config/initializers/spree_permissions.rb
+++ config/initializers/spree_permissions.rb
@@ -7,6 +7,17 @@ Rails.application.config.to_prepare do
     Spree::PermissionSets::DashboardDisplay,
     Spree::PermissionSets::OrderManagement,
     Spree::PermissionSets::ProductManagement,
-    Spree::PermissionSets::UserDisplay
+    Spree::PermissionSets::UserDisplay,
+    Spree::PermissionSets::UserManagement,
+    Spree::PermissionSets::ConfigurationManagement,
+    Spree::PermissionSets::Base,
+    Spree::PermissionSets::OrderDisplay,
+    Spree::PermissionSets::ProductDisplay,
+    Spree::PermissionSets::DefaultCustomer,
+    Spree::PermissionSets::PromotionManagement,
+    Spree::PermissionSets::StockDisplay,
+    Spree::PermissionSets::StockManagement,
+    Spree::PermissionSets::RoleManagement,
+    Spree::PermissionSets::SuperUser 
   ])
 end
